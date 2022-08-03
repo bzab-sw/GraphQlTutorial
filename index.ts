c9dc35ecb025252c11f7852e3e2fd10551d169a4
Blazej Zabialowicz <blazej.zabialowicz@solarwinds.com>
2022-08-03 13:26:15 +0200

    Separating root

--- index.ts
+++ index.ts
@@ -1,17 +1,12 @@
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
 import { loadSchema } from '@graphql-tools/load';
-import nodes from './resolvers/nodes';
+import root from './resolvers/root';
 import { Server } from './server';
 
 // Load schema from the file
 const schema = await loadSchema('./schemas/**/*.graphql', {
   loaders: [new GraphQLFileLoader()]
 })
-
-// The root provides a resolver function for each API endpoint
-const root = {
-  nodes
-};
 
 var server = new Server(schema, root);
 server.run();
---
+++ resolvers/root.ts
@@ -0,0 +1,8 @@
+import nodes from "./nodes";
+
+// The root provides a resolver function for each API endpoint
+const root = {
+    nodes
+  };
+
+export default root
