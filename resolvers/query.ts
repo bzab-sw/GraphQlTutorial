59a9cff21279a51a25cf6609bf405c937fac7966
Blazej Zabialowicz <blazej.zabialowicz@solarwinds.com>
2022-08-03 15:23:30 +0200

    Fix merge

--- resolvers/query.ts
+++ resolvers/query.ts
@@ -1,8 +1,10 @@
+import configs from "./configs";
 import nodes from "./nodes";
 
 // The root provides a resolver function for each API endpoint
 const Query = {
-    nodes
+    nodes,
+    configs
   };
 
 export default Query
