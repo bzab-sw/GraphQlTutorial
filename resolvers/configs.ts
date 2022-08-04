b3e3adbe04d2230c66047f745cff4f31f3cdbdb1
Blazej Zabialowicz <blazej.zabialowicz@solarwinds.com>
2022-08-04 12:56:47 +0200

    8. Change node id to node

--- resolvers/configs.ts
+++ resolvers/configs.ts
@@ -1,6 +1,31 @@
 import { configDalInstance } from "../dal/configsDal";
+import { nodesDalInstance } from "../dal/nodesDal";
 import { Config } from "../model/config";
+import { Node } from "../model/node";
 
-export default function configs(): Config[] {
-    return configDalInstance.getAll();
+class GqlConfigWrap {
+    constructor(private config: Config) {
+
+    }
+
+    public get id(): number {
+        return this.config.id;
+    }
+
+    public get title(): string {
+        return this.config.title;
+    }
+
+    public get downloadTime(): Date {
+        return this.config.downloadTime;
+    }
+
+    public get node(): Node {
+        return nodesDalInstance.get(this.config.nodeId);
+    }
+}
+
+export default function configs(): GqlConfigWrap[] {
+    return configDalInstance.getAll()
+        .map(c => new GqlConfigWrap(c));
 }
