9bca73bd7c376af4b18483d065812b0eb4ec7d45
Blazej Zabialowicz <blazej.zabialowicz@solarwinds.com>
2022-08-05 09:50:44 +0200

    Request info

--- resolvers/query.ts
+++ resolvers/query.ts
@@ -1,10 +1,12 @@
 import { configs } from "./configs";
 import { nodes}  from "./nodes";
+import { requestInfo } from "./requestInfo";
 
 // The root provides a resolver function for each API endpoint
 const Query = {
     nodes,
-    configs
+    configs,
+    requestInfo
   };
 
 export default Query
---
+++ resolvers/requestInfo.ts
@@ -0,0 +1,29 @@
+export interface RequestInfo {
+    host?: string
+    method?: string
+    headers?: Header[]
+}
+
+export interface Header {
+    key: string
+    value: string
+}
+
+export function requestInfo(obj: any, args: any, context: any, info: any): RequestInfo {
+    const reqHeaders = context.headers;
+    const keys = Object.getOwnPropertyNames(reqHeaders);
+    const headers: Header[] = [];
+
+    for (let key of keys) {
+        headers.push({
+            key: key,
+            value: reqHeaders[key]
+        });
+    } 
+
+    return {
+        host: context.hostname,
+        method: context.method,
+        headers: headers
+    };
+}
