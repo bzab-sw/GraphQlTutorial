f05979ae35462fac008d6bb961150915e9c809e4
Blazej Zabialowicz <blazej.zabialowicz@solarwinds.com>
2022-08-05 11:20:13 +0200

    Better info type

--- resolvers/nodes.ts
+++ resolvers/nodes.ts
@@ -1,6 +1,7 @@
+import { GraphQLResolveInfo } from 'graphql';
 import { nodesDalInstance } from '../dal/nodesDal';
 import { Node } from '../model/node'
 
-export default function nodes(obj: any, args: any, context: any, info: any): Node[] {
+export default function nodes(obj: any, args: any, context: any, info: GraphQLResolveInfo): Node[] {
     return nodesDalInstance.getAll();
 }
