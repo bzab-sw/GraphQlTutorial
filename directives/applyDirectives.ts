f072e18304ba7c89476bbf8457cdb3c8e9048556
Blazej Zabialowicz <blazej.zabialowicz@solarwinds.com>
2022-08-05 10:39:11 +0200

    Better directives

--- directives/applyDirectives.ts
+++ directives/applyDirectives.ts
@@ -1,7 +1,17 @@
 import { GraphQLSchema } from "graphql";
 
+export type DirectiveHandler = (schema: GraphQLSchema) => GraphQLSchema; 
+
+const handlers: DirectiveHandler[] = [
+
+];
+
 export function applyDirectives(schema: GraphQLSchema): GraphQLSchema {
     let fixed = schema;
+
+    for (const handler of handlers) {
+        fixed = handler(fixed);
+    }
 
     return fixed;
 }
